e3eeb7163a0448ff1b0f26b65b6948de16e4cc8c
Sabid <sajjadsabid0@gmail.com>
2025-09-08 22:17:09 +0600

    Added footer section

--- script.js
+++ script.js
@@ -1,1 +1,125 @@
-const plantsAPI = ""
+const plantsAPI = "https://openapi.programming-hero.com/api/plants";
+const categoriesAPI = "https://openapi.programming-hero.com/api/categories";
+const plantsContainer = document.getElementById("plants-container");
+const categoryList = document.getElementById("category-list");
+const cartItems = document.getElementById("cart-items");
+const totalPrice = document.getElementById("total");
+
+let plants = [];
+let categories = [];
+let cart = [];
+
+async function loadPlants() {
+    const res = await fetch(plantsAPI);
+    const data = await res.json();
+    plants = data.plants;
+    renderPlants(plants);
+}
+
+async function loadCategories() {
+    const res = await fetch(categoriesAPI);
+    const data = await res.json();
+    categories = data.categories;
+    categories.forEach(cat => {
+        const li = document.createElement("li");
+        li.textContent = cat.category_name;
+        li.className = "cursor-pointer p-2 rounded hover:bg-green-200";
+        li.dataset.category = cat.category_name;
+        li.addEventListener("click", () => filterByCategory(cat.category_name));
+        categoryList.appendChild(li);
+    });
+}
+
+
+
+
+
+function renderPlants(plantsDate) {
+    plantsContainer.innerHTML = "";
+    plantsDate.forEach(plant => {
+        const card = document.createElement("div");
+        card.className = "bg-white p-4 rounded shadow flex flex-col  w-[318.94px] h-[296px] max-sm:w-[100%] max-sm:h-auto";
+        card.innerHTML = `
+        <img src="${plant.image}" alt="${plant.name}" class="h-32 w-full object-cover rounded">
+        <h1 class= "font-semibold mt-2">${plant.name}</h1>
+        <p class= "text-sm text-gray-700">${plant.description.slice(0, 60)}...</p>
+        <p class="text-green-600 text-sm mt-1">${plant.category}</p>
+        <div class="flex justify-between items-center mt-auto pt-2">
+            <span class="font-bold">ট ${plant.price}</span>
+            <button class="cursor-pointer bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700" data-id="${plant.id}">
+              Add to Cart
+            </button>
+        </div>
+        `;
+        const btn = card.querySelector("button");
+        btn.addEventListener('click', () => {
+            alert (`${plant.name} has been added to the card`)
+            addTocart(plant)});
+        plantsContainer.appendChild(card);
+    });
+
+}
+
+
+function renderCard() {
+    cartItems.innerHTML = '';
+    let total = 0;
+    cart.forEach(item => {
+        total += item.price * item.qty;
+        const div = document.createElement("div");
+        div.className = "flex justify-between items-center mb-2 bg-[#CFF0DC] rounded px-4 py-2"
+        div.innerHTML = `
+        <div>
+            <h2>${item.name}</h2>
+            <p class="font-bold pt-1">ট ${item.price} x ${item.qty}</p>
+        </div>
+        <button class="text-red-600">✕</button>
+        `;
+        div.querySelector("button").addEventListener('click', () => removeFromCard(item.id));
+        cartItems.appendChild(div)
+    })
+    totalPrice.textContent = total;
+}
+
+
+function filterByCategory(category){
+    document.querySelectorAll("#category-list li").forEach(li => {
+        li.classList.remove("bg-green-600", "text-white");
+    });
+    event.target.classList.add("bg-green-600", "text-white");
+    if(category === "All Tress") {
+        renderPlants(plants);
+    }
+    else{
+        renderPlants(plants.filter(p => p.category === category));
+    }
+}
+
+function addTocart(plant) {
+    const exists = cart.find(item => item.id === plant.id);
+    if (exists) {
+        exists.qty += 1;
+    }
+    else {
+        cart.push({ ...plant, qty: 1 });
+    }
+    renderCard();
+}
+
+function removeFromCard(id) {
+    cart = cart.filter(item => item.id !== id);
+    renderCard()
+}
+
+
+document.getElementById('all-tress-name').addEventListener('click', () => {
+    document.querySelectorAll("#category-list li").forEach(li => {
+        li.classList.remove("bg-green-600", "text-white");
+    });
+    event.target.classList.add("bg-green-600", "text-white");
+    renderPlants(plants);
+})
+
+
+loadPlants();
+loadCategories();
